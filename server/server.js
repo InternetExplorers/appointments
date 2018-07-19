2c9cee2d4d40abcfbb93a86e1a8ce9ecbf6cb0a2
Jordan Bice <jmbice@gmail.com>
2018-07-18 19:05:06 -0700

    Serving static files

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+// app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(3000, () => console.log('Nice Jordan, app listening on port 3000!'));
