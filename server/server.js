62436cc4ee8150875f61e76159102af8be6a663d
Jordan Bice <jmbice@gmail.com>
2018-07-18 19:10:09 -0700

    Add bodyParser to dependencies/server adjust port#

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
+const PORT = 3003;
 const app = express();
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
 // app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Nice Jordan, app listening on port 3000!'));
+app.listen(PORT, () => console.log(`Nice Jordan, app listening on port ${PORT}!`));
