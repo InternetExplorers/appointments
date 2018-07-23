8da4e86b75b79bc02a92ab51822989d139bd5ac4
Jordan Bice <jmbice@gmail.com>
2018-07-23 16:18:14 -0700

    Add routing and connect DB calls to server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const helper = require('../db/dbHelpers.js');
 
 const PORT = 3003;
 const app = express();
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
-// app.get('/', (req, res) => res.send('Hello World!'));
+app.use((req, res, next) => {
+  console.log(req.body, req.path);
+  next();
+});
+
+app.get('/appointments/:id', (req, res) => {
+  helper.getBusinessInfo({ id: req.params.id }, (err, success) => {
+    if (err) {
+      res.status(404).send();
+    } else {
+      res.status(200).send(success);
+    }
+  });
+});
+
 
 app.listen(PORT, () => console.log(`Nice Jordan, app listening on port ${PORT}!`));
