995b141e745b533590847de48999142e7b3b1ae6
Jordan Bice <jmbice@gmail.com>
2018-07-28 14:55:47 -0700

    Add snapshot tests

--- tests/LevelOne.test.js
+++ tests/LevelOne.test.js
@@ -1,6 +1,7 @@
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow } from 'enzyme';
 import * as enzyme from 'enzyme';
+import toJson from 'enzyme-to-json';
 import React from 'react';
 import LevelOne from '../client/components/LevelOne.jsx';
 import getGuestCount from '../client/components/helperFunctions/getGuestCount.jsx';
@@ -8,6 +9,23 @@ import getGuestCount from '../client/components/helperFunctions/getGuestCount.js
 enzyme.configure({ adapter: new Adapter() });
 
   describe('LevelOne, a child component', () => {
+    it('should render 3 dropdown menus, and a submit button', () => {
+      const levelOneComponent = shallow(
+        <LevelOne
+          nextTwoWeeks={["July 26, 2018", "July 27, 2018", "July 28, 2018", "July 29, 2018", "July 30, 2018", "July 31, 2018", "August 1, 2018", "August 2, 2018", "August 3, 2018", "August 4, 2018", "August 5, 2018", "August 6, 2018", "August 7, 2018", "August 8, 2018"]}
+          times={["9:30 AM", "10:30 AM", "11:30 AM", "12:30 PM", "1:30 PM", "2:30 PM", "3:30 PM"]}
+          max={5}
+          change={(() => { null })()}
+          next={(() => { null })()}
+          count={'2'}
+          date={'July 26, 2018'}
+          time={'9:30 AM'}
+        />
+      );
+
+      expect(toJson(levelOneComponent)).toMatchSnapshot();
+    });
+
     it('getGuestCount should return an array of numbers, or null', () => {
       expect(getGuestCount(100).length).toEqual(12);
       expect(getGuestCount(50).length).toEqual(12);
