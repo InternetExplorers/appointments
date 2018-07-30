5c94e1a718718eeb3e816bc10e8f7d4564d87f7e
Jordan Bice <jmbice@gmail.com>
2018-07-30 16:43:29 -0700

    Add middleware for cross origin requests

--- server/server.js
+++ server/server.js
@@ -7,6 +7,12 @@ const PORT = 3003;
 const app = express();
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
