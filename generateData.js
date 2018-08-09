f0234f0d470020557f022be90ab5fbb109e48657
Zach Coursey <zcoursey22@gmail.com>
2018-08-09 13:45:47 -0700

    Can generate 10M records into csv files

---
+++ generateData.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+const adj = [
+  'Tasty',
+  'Gorgeous',
+  'Awesome',
+  'Unbranded',
+  'Handcrafted',
+  'Small',
+  'Incredible',
+  'Ergonomic',
+  'Refined',
+  'Licensed',
+];
+
+const append = (data) => {
+  const chunks = [];
+  let prevIdx = 0;
+  for (let j = 0; j <= data.length; j += 10000) {
+    chunks.push(data.slice(prevIdx, j).join('\n'));
+    prevIdx = j;
+  }
+  for (let i = 0; i < 100; i += 1) {
+    fs.appendFileSync(`data${i === 0 ? '' : i}.csv`, chunks[i]);
+  }
+};
+
+const makeUniqueBusinesses = (rounds) => {
+  const storage = [];
+  const makeCombos = (counter, combo = []) => {
+    if (!counter) {
+      storage.push(combo);
+    } else {
+      for (let i = 0; i < adj.length; i += 1) {
+        const element = adj[i];
+        makeCombos(counter - 1, combo.concat(element));
+      }
+    }
+  };
+  makeCombos(rounds);
+  append(storage);
+};
+
+makeUniqueBusinesses(7);
